f26b604088216129bd5845f5aa4b50a2a52d504a
Martin Petluš <martinpetlus@gmail.com>
2023-03-12 11:26:06 +0100

    adjust files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,60 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
-const inter = Inter({ subsets: ['latin'] })
+const IconSize = 40;
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex h-screen flex-col bg-zinc-900 text-red-500">
+      <div className="flex flex-grow">
+        <div className="m-auto">
+          <h1 className="mb-4 text-4xl font-bold">
+            <span className={inter.className}>Martin Petluš</span>
+          </h1>
+          <p className="grid grid-cols-4">
+            <a
+              href="https://github.com/martinpetlus"
+              title="My GitHub profile"
+              className="mx-auto"
+            >
+              <FaGithub className="text-red-500" size={IconSize} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/martinpetlus/"
+              title="My LinkedIn profile"
+              className="mx-auto"
+            >
+              <FaLinkedin className="text-red-500" size={IconSize} />
+            </a>
+            <a
+              href="https://twitter.com/martinpetlus"
+              title="My Twitter profile"
+              className="mx-auto"
+            >
+              <FaTwitter className="text-red-500" size={IconSize} />
+            </a>
+            <a
+              href="https://sledujbyty.sk/"
+              title="Sleduj Byty"
+              className="mx-auto"
+            >
+              <Image
+                src="/logo.svg"
+                alt="Sleduj Byty"
+                width={IconSize}
+                height={IconSize}
+                priority
+              />
+            </a>
+          </p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+      <div className="py-2 text-center">
+        <span className={inter.className}>© {new Date().getFullYear()}</span>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
