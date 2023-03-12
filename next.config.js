6269ab3978f088153774c94b208398298ee5a6c0
Martin Petluš <martinpetlus@gmail.com>
2023-03-12 20:20:23 +0100

    try fix images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  // FIXME: remove once fixed in GitHub Action
+  assetPrefix: '/martinpetlus.sk/',
   images: { unoptimized: true },
 };
 
